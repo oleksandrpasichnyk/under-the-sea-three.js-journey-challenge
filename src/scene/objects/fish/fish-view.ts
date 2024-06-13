5ff04b06c5113acbf56a79665a282236b4edd26e
Oleksandr Pasichnyk <oleksandr.pasichnyk.wokr@gmail.com>
2024-06-14 00:45:30 +0300

    updated moving

--- src/scene/objects/fish/fish-view.ts
+++ src/scene/objects/fish/fish-view.ts
@@ -8,7 +8,21 @@ const enum KEYS {
   DOWN = 'KeyS',
   LEFT = 'KeyA',
   RIGHT = 'KeyD',
+  SHIFT = 'ShiftLeft',
+  SPACE = 'Space',
 }
+
+const RACING_CONFIG = {
+  maxSpeed: 50,
+  maxTurboSpeed: 100,
+  acceleration: 6,
+  turboAcceleration: 12,
+  deceleration: 5,
+  rotationSpeed: 3,
+  rotationLerp: 0.03,
+  breakSpeed: 20,
+};
+
 
 export default class Fish extends THREE.Group {
   private view!: THREE.Object3D;
@@ -20,12 +34,13 @@ export default class Fish extends THREE.Group {
   private _speed: number = 0;
   private _angle: THREE.Euler = new THREE.Euler();
 
-  private _maxSpeed: number = 50;
-  private _acceleration: number = 7;
-  private _deceleration: number = 6;
-  private _rotationSpeed: number = 3;
-  private _rotationLerp: number = 0.2;
-  private _breakSpeed: number = 20;
+  private _maxSpeed: number = RACING_CONFIG.maxSpeed;
+  private _acceleration: number = RACING_CONFIG.acceleration;
+
+  private _deceleration: number = RACING_CONFIG.deceleration;
+  private _rotationSpeed: number = RACING_CONFIG.rotationSpeed;
+  private _rotationLerp: number = RACING_CONFIG.rotationLerp;
+  private _breakSpeed: number = RACING_CONFIG.breakSpeed;
 
   private _realSpeed: number = 0;
   private _boundingBox!: THREE.Vector3;
@@ -75,10 +90,8 @@ export default class Fish extends THREE.Group {
 
     this._boundingBox = new THREE.Box3().setFromObject(this.view).getSize(new THREE.Vector3());
 
-    console.log(this._boundingBox)
-
     const line = this.line = new THREE.Line(new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -10)]), new THREE.LineBasicMaterial({ color: 0xff0000 }));
-    
+
     setTimeout(() => {
       this.parent?.add(line);
     }, 500);
@@ -92,7 +105,7 @@ export default class Fish extends THREE.Group {
     view.scale.set(s, s, s);
     view.rotation.y = Math.PI;
 
-    if(asset.animations.length !== 0){
+    if (asset.animations.length !== 0) {
       const mixer = this.mixer = new FishAnimationsController(view, asset.animations);
 
       mixer.playAnimation(FISH_ANIMATION_TYPE.SwimmingNormal);
@@ -107,41 +120,51 @@ export default class Fish extends THREE.Group {
     document.addEventListener('keydown', (event) => {
       this.keyStates[event.code] = true;
     });
-    
+
     document.addEventListener('keyup', (event) => {
       this.keyStates[event.code] = false;
     });
   }
 
-  public update(dt: number) {
-    if (this.keyStates[KEYS.UP]) {
-        this._speed += (this._speed > 0 ? this._acceleration : this._breakSpeed) * dt;
-        this._speed = Math.min(this._speed, this._maxSpeed);
-    } else if (this.keyStates[KEYS.DOWN]) {
-        // this._speed -= this._breakSpeed * dt;
-        this._speed -= (this._speed > 0 ? this._breakSpeed : this._acceleration) * dt;
-
-        this._speed = Math.max(this._speed, -this._maxSpeed);
+  private checkShift() {
+    if (this.keyStates[KEYS.SHIFT]) {
+      this._maxSpeed = RACING_CONFIG.maxTurboSpeed;
+      this._acceleration = RACING_CONFIG.turboAcceleration;
     } else {
-        if (this._speed > 0) {
-            this._speed -= this._deceleration * dt;
-            this._speed = Math.max(this._speed, 0);
-        } else if (this._speed < 0) {
-            this._speed += this._deceleration * dt;
-            this._speed = Math.min(this._speed, 0);
-        }
+      this._maxSpeed = RACING_CONFIG.maxSpeed;
+      this._acceleration = RACING_CONFIG.acceleration;
+    }
+  }
+
+  private checkControls(dt: number) {
+    if (this.keyStates[KEYS.UP]) {
+      this._speed += (this._speed > 0 ? this._acceleration : this._breakSpeed) * dt;
+      this._speed = Math.min(this._speed, this._maxSpeed);
+    } else if (this.keyStates[KEYS.DOWN]) {
+      this._speed -= (this._speed > 0 ? this._breakSpeed : this._acceleration) * dt;
+      this._speed = Math.max(this._speed, -this._maxSpeed);
+    } else {
+      if (this._speed > 0) {
+        this._speed -= this._deceleration * dt;
+        this._speed = Math.max(this._speed, 0);
+      } else if (this._speed < 0) {
+        this._speed += this._deceleration * dt;
+        this._speed = Math.min(this._speed, 0);
+      }
     }
 
     if (this.keyStates[KEYS.LEFT]) {
-        this._angle.y += this._rotationSpeed * dt;
+      this._angle.y += this._rotationSpeed * dt;
     } else if (this.keyStates[KEYS.RIGHT]) {
-        this._angle.y -= this._rotationSpeed * dt;
+      this._angle.y -= this._rotationSpeed * dt;
     }
-    
+  }
+
+  private updatePosition(dt: number) {
     if (this._angle.y !== 0) {
       this.rotation.y = THREE.MathUtils.lerp(this.rotation.y, this._angle.y, this._rotationLerp);
     }
-    
+
     const prewPos = this.position.clone();
 
     const direction = new THREE.Vector3(0, 0, -1).applyQuaternion(this.quaternion).multiplyScalar(this._speed * dt);
@@ -150,13 +173,20 @@ export default class Fish extends THREE.Group {
     this._realSpeed = Math.round(this.position.distanceTo(prewPos) / dt);
     this.realSpeedDisplay?.updateDisplay();
 
-    this.mixer?.setTimeScaleMoveSpeed(5 * this._realSpeed/this._maxSpeed);
-    this.mixer?.update(dt);
-
-
     this.line.position.copy(this.position);
     this.line.lookAt(this.position.clone().add(direction.clone().add(new THREE.Vector3(0, -0.002, 0)).negate()));
   }
-  
 
+  private updateAnimation(dt: number) {
+    this.mixer?.setTimeScaleMoveSpeed(5 * this._realSpeed / this._maxSpeed);
+    this.mixer?.update(dt);
+  }
+
+  public update(dt: number) {
+    this.checkShift();
+    this.checkControls(dt);
+
+    this.updatePosition(dt);
+    this.updateAnimation(dt);
+  }
 }
